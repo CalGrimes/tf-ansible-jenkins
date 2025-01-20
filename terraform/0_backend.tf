3b17f4442a9362bf9ec0ceb0451c636bd1384d89
CalGrimes <cal.grimes@outlook.com>
2025-01-20 22:12:47 +0000

    [fix] Correct bucket name

--- terraform/0_backend.tf
+++ terraform/0_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "cgrim-s3-tfstate"
+    bucket         = "cgrim-dev-s3-tfstate"
     key            = "terraform/terraform.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "terraform-lock-table"
