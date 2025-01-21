042c49600112a8d7f6f962891a260e50cce53cea
CalGrimes <cal.grimes@outlook.com>
2025-01-21 17:18:48 +0000

    [feat] Add secrets to only allow my_ip ingress

--- terraform/0_variables.tf
+++ terraform/0_variables.tf
@@ -7,3 +7,8 @@ variable "prefix" {
   description = "Prefix will be included in the name of most resources."
   default = ""
 }
+
+variable "my_ip" {
+  description = "Your IP address."
+  default = ""
+}
--- terraform/1_networking.tf
+++ terraform/1_networking.tf
@@ -25,21 +25,21 @@ resource "aws_security_group" "sg_jenkins" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
     from_port   = 50000
     to_port     = 50000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
   }
 
 
