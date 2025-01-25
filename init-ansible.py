72a6a2a378e5d01278b37cac78adeea3b472ed22
CalGrimes <cal.grimes@outlook.com>
2025-01-25 18:05:29 +0000

    Create init-ansible.py
    Move python code to file

---
+++ init-ansible.py
@@ -0,0 +1,20 @@
+import os
+
+public_ip = ""
+inventory = ""
+
+# Read public_ip.txt
+with open('/tmp/public_ip.txt') as file:
+  public_ip = file.read()
+  print("Python public_ip", public_ip)
+
+# Read the inventory file
+with open('ansible/inventory', 'r') as file:
+  inventory = file.read()
+  
+# Substitute <host_ip> with the actual IP
+inventory = inventory.replace('<host_ip>', public_ip)
+  
+# Write the modified content back to the inventory file
+with open('ansible/inventory', 'w') as file:
+  file.write(inventory)
