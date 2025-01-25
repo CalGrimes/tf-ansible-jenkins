545c00b46271303c00bd778346b46b3dabcde33f
CalGrimes <calwgrimes29@gmail.com>
2025-01-25 01:15:37 +0000

    [refactor] Move ansible commands to bash file to avoid github workflow vars

---
+++ init-ansible.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+export PUBLIC_IP=$(terraform output -raw public_ip)
+
+cd ..
+
+sed "s|<host_ip>|$PUBLIC_IP|g" ansible/inventory
+
+pip3 install --user ansible
+
+cat ansible/inventory
+
+cd ansible && ansible-playbook --private-key /tmp/private_key.pem -i inventory jenkinsPlaybook.yaml
