112d2723be239fce35f35281c8dac31ba283b200
CalGrimes <calwgrimes29@gmail.com>
2025-01-25 00:15:29 +0000

    [chore] Remove acl

--- terraform/3_s3.tf
+++ terraform/3_s3.tf
@@ -1,7 +1,5 @@
 resource "aws_s3_bucket" "s3_key_bucket" {
   bucket = "${var.prefix}-instance-key"
-  acl    = "private"
-
   tags = {
     Name        = "${var.prefix}-instance-key"
     Environment = "Dev"
